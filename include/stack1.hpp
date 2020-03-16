d56242683bdd49941d69158fd0b90eb77ab9eedb
zoolohol <58566741+zoolohol@users.noreply.github.com>
2020-03-16 13:21:24 +0300

    Update stack1.hpp

--- include/stack1.hpp
+++ include/stack1.hpp
@@ -4,93 +4,55 @@
 #define INCLUDE_STACK1_HPP_
 #include <memory>
 #include <iostream>
-#include <utility>
+#include <type_traits>
+#include <exception>
 template <typename T>
 class stack1
 {
-private:
-    unsigned int *size = new unsigned int;
-    std::unique_ptr<T[]> arr;
+ struct Node {
+        T data;
+        Node *next;
+    };
+    Node *head;
+
 public:
-    void push(T&& value);
-    void push(const T& value);
-    void pop();
-    const T& head() const;
-    stack1();
-    ~stack1();
-    stack1(stack1&& stack);
-    stack1<T>& operator=(stack1<T>&& stack);
-};
+    Stack() = default;
 
-template <typename T>
-stack1<T>::stack1(){
-    *(size) = 0;
-    arr.reset(new T[*(size)]);
-}
-
-template <typename T>
-stack1<T>& stack1<T>::operator=(stack1<T>&& stack) {
-    if (std::move(stack) != this) {
-        std::swap(size, stack.size);
-        std::swap(this->arr, stack.arr);
+    ~Stack() {
+        while (head) {
+            auto *act = head;
+            head = act->next;
+            delete act;
+        }
     }
 
-    return *this;
-}
-template <typename T>
-stack1<T>::stack1(stack1&& stack){
-    std::swap(*size, *stack.size);
-    std::swap(arr, stack.arr);
-}
-
-template <typename T>
-stack1<T>::~stack1(){
-    delete size;
-    arr.reset();
-}
-
-template <typename T>
-void stack1<T>::pop() {
-    if (*(size) == 0) {
-        std::cout << "Массив пуст";
-        return;
+    void push(T&& value) {
+        if (head == nullptr) {
+            auto node = new Node{std::move(value), nullptr};
+            head = node;
+        } else {
+            auto node = new Node{std::move(value), head};
+            head = node;
+        }
     }
-    (*(size))--;
-    std::unique_ptr<T[]> new_arr(new T[*(size)]);
-    for (unsigned int i = 0; i < *(size); ++i){
-        new_arr.get()[i]  =  arr.get()[i];
+
+    const T& Head() {
+        if (!head)
+            throw std::exception();
+        return head->data;
     }
-    arr.swap(new_arr);
-    new_arr.reset();
-}
-
-template <typename T>
-const T& stack1<T>::head() const{
-        return arr.get()[*(size) - 1];
-}
-
-template <typename T>
-void stack1<T>::push(const T& value) {
-    (*(size))++;
-std::unique_ptr<T[]> new_arr(new T[*(size)]);
- for (unsigned int i = 0; i < (*(size)-1); ++i){
-     new_arr[i] = arr.get()[i];
- }
- new_arr.get()[*(size)-1] = value;
- arr.swap(new_arr);
-    new_arr.reset();
-}
-
-template <typename T>
-void stack1<T>::push(T&& value) {
-    (*(size))++;
-    std::unique_ptr<T[]> new_arr(new T[*(size)]);
-    for (unsigned int i = 0; i < (*(size)-1); ++i){
-        new_arr[i] = arr.get()[i];
+    void push(const T& value) {
+        auto node = new typename Stack<T>::Node{value, Stack<T>::head};
+        Stack<T>::head = node;
     }
-    new_arr.get()[*(size)-1] = std::move(value);
-    arr.swap(new_arr);
-    new_arr.reset();
+
+    void pop() {
+        if (Stack<T>::head)
+            Stack<T>::head = Stack<T>::head->next;
+        else
+            throw std::exception();
+    }
+    
 }
 
 #endif // INCLUDE_STACK1_HPP_
